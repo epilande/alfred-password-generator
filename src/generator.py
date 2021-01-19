2ddc0e2eea040154eaff9253901a9d7e89f8fe65
Emmanuel Pilande <epilande@gmail.com>
2021-01-19 00:20:25 -0800

    :sparkles: Add combination options

--- src/generator.py
+++ src/generator.py
@@ -6,18 +6,41 @@ import secrets
 import json
 
 defaultLength = os.environ['defaultLength']
-punctuation = os.environ['punctuation']
-digits = os.environ['digits']
 letters = os.environ['letters']
+digits = os.environ['digits']
+punctuation = os.environ['punctuation']
 
 length = int(sys.argv[1]) if len(sys.argv) > 1 else int(defaultLength)
-password = ''.join(secrets.choice(letters + digits + punctuation)
-                   for i in range(length))
 
-print(json.dumps({'items': [
+combinations = [
     {
         'title': f"Generate password {length} characters",
+        'combo': letters + digits + punctuation
+    },
+    {
+        'title': f"{length} characters - Letters",
+        'combo': letters
+    },
+    {
+        'title': f"{length} characters - Letters + Digits",
+        'combo': letters + digits
+    },
+    {
+        'title': f"{length} characters - Letters + Punctuation",
+        'combo': letters + punctuation
+    }
+]
+
+
+def item(d):
+    password = ''.join(secrets.choice(d['combo']) for i in range(length))
+    return {
+        'title': d['title'],
         'subtitle': f"{password}",
         'arg': f"{password}",
     }
-]}))
+
+
+items = list(map(item, combinations))
+
+print(json.dumps({'items': items}))
