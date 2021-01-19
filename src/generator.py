b082751486b2a2eb8443792d86f0adcca36af92d
Emmanuel Pilande <epilande@gmail.com>
2021-01-18 23:26:20 -0800

    :sparkles: Use workflow env variables

--- src/generator.py
+++ src/generator.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import secrets
-import string
 import json
 
-length = int(sys.argv[1]) if len(sys.argv) > 1 else 16
-alphabet = string.ascii_letters + string.digits + string.punctuation
-password = ''.join(secrets.choice(alphabet) for i in range(length))
+defaultLength = os.environ['defaultLength']
+punctuation = os.environ['punctuation']
+digits = os.environ['digits']
+letters = os.environ['letters']
+
+length = int(sys.argv[1]) if len(sys.argv) > 1 else int(defaultLength)
+password = ''.join(secrets.choice(letters + digits + punctuation)
+                   for i in range(length))
 
 print(json.dumps({'items': [
     {
